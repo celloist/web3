15452f03c4016b9c624deebfbaa73f32afec436b
alhric <alhriclacle@gmail.com>
2015-06-25 13:35:37 +0200

    df

--- resources/views/partials/breadcrumbs.blade.php
+++ resources/views/partials/breadcrumbs.blade.php
@@ -3,9 +3,10 @@
         <nav class="breadcrumbs">
             @foreach ($breadcrumbs as $breadcrumb)
                 @if (!$breadcrumb->last)
-                    <a class="current" href="#">{{ $breadcrumb->title }}</a>
-                @else
+
                     <a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
+                @else
+                    <a class="current" href="#">{{ $breadcrumb->title }}</a>
                 @endif
             @endforeach
         </nav>
